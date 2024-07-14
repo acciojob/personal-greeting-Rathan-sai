400473d83240e79c80ae1af4e5d9fa278d83b328
Rathansai45 <rathansai45@gmail.com>
2024-07-14 12:26:59 +0530

    solution 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
+
+  // var text = "ram";
+
+  const handleChange = (e) => {
+    console.log("Hello " + e.target.value + "!");
+    if (e.target.value === "") {
+      setText("");
+      return;
+    }
+    setText("Hello " + e.target.value + "!");
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input onChange={handleChange}></input>
+      <p>{text}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
